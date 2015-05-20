7c5786a0320f503ba2b3c925894c234514738dc4
karen-lim <karen.lim@sypherlabs.com>
2015-05-20 14:13:52 +0800

    Edited videoTracking.js

--- scripts/videoTracking.js
+++ scripts/videoTracking.js
@@ -1,4 +1,4 @@
-videojs('#thevideo', {}, function() {
+videojs('#iframe', {}, function() {
 	var player = this;
 	player.ga({'gaLibrary' : 'analytics.js'}); // "load the plugin, by defaults tracks everything!!"
 });	
